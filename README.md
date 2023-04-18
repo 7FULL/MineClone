853d790eccfd2f8177a7dd90400fbdbc1854d85a
7FULL <phgfull@gmail.com>
2023-04-18 11:56:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MineClone
+ 
---
+++ WorldRenderer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRenderer : MonoBehaviour
+{
+    public GameObject chunkPrefab;
+    public GameObject chunkFather;
+    public Queue<ChunkRenderer> chunkPool = new Queue<ChunkRenderer>();
+
+    public void Clear(WorldData worldData)
+    {
+        foreach (ChunkRenderer item in worldData.chunkDictionary.Values)
+        {
+            Destroy(item.gameObject);
+        }
+        chunkPool.Clear();
+    }
+
+
+    internal ChunkRenderer RenderChunk(WorldData worldData, Vector3Int position, MeshData meshData)
+    {
+        ChunkRenderer newChunk = null;
+        if(chunkPool.Count > 0)
+        {
+            newChunk = chunkPool.Dequeue();
+            newChunk.transform.position = position;
+        }
+        else
+        {
+            GameObject chunkObject = Instantiate(chunkPrefab, position, Quaternion.identity);
+            chunkObject.transform.SetParent(chunkFather.transform);
+            newChunk = chunkObject.GetComponent<ChunkRenderer>();
+        }
+
+        newChunk.InitializeChunk(worldData.chunkDataDictionary[position]);
+        newChunk.UpdateChunk(meshData);
+        newChunk.gameObject.SetActive(true);
+        return newChunk;
+    }
+
+    public void RemoveChunk(ChunkRenderer chunk)
+    {
+        chunk.gameObject.SetActive(false);
+        chunkPool.Enqueue(chunk);
+    }
+}
