610aa979895c45f2383d877e426ee00117846924
7FULL <phgfull@gmail.com>
2023-04-24 22:14:43 +0200

    Inventario con crafteo sin dropeo

--- CraftingSlot.cs
+++ CraftingSlot.cs
@@ -9,11 +9,15 @@ public class CraftingSlot : MonoBehaviour, IDropHandler, Slot
     CraftingManager cf;
     [SerializeField] private int slot;
 
+    private DragDropItem item;
+
+    private GameObject gameObjectItem;
+    
     private void Awake()
     {
         cf = GetComponentInParent<CraftingManager>();
     }
-
+    
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag != null)
@@ -23,29 +27,65 @@ public class CraftingSlot : MonoBehaviour, IDropHandler, Slot
 
             DragDropItem item = eventData.pointerDrag.GetComponent<DragDropItem>();
 
+            this.item = item;
+
+            gameObjectItem = eventData.pointerDrag;
+
+            update(eventData);
+            
+            cf.UpdateOutputSlot();
+        }
+    }
+
+    public void update(PointerEventData eventData)
+    {
+        if (item != null)
+        {
             if (slot == 0)
             {
-                cf.item_02GameObject = eventData.pointerDrag;
+                cf.item_02GameObject = gameObjectItem;
                 cf.item_02 = item.item;
                 cf.item_02Much = item.itemMuch;
             }else if (slot == 1)
             {
-                cf.item_12GameObject = eventData.pointerDrag;
+                cf.item_12GameObject = gameObjectItem;
                 cf.item_12 = item.item;
                 cf.item_12Much = item.itemMuch;
             }else if (slot == 2)
             {
-                cf.item_01GameObject = eventData.pointerDrag;
+                cf.item_01GameObject = gameObjectItem;
                 cf.item_01 = item.item;
                 cf.item_01Much = item.itemMuch;
             }else if (slot == 3)
             {
-                cf.item_11GameObject = eventData.pointerDrag;
+                cf.item_11GameObject = gameObjectItem;
                 cf.item_11 = item.item;
                 cf.item_11Much = item.itemMuch;
             }
-            
-            cf.UpdateOutputSlot();
+        }
+        else
+        {
+            if (slot == 0)
+            {
+                cf.item_02GameObject = null;
+                cf.item_02 = GameManager.instance.defaultItem;
+                cf.item_02Much = 0;
+            }else if (slot == 1)
+            {
+                cf.item_12GameObject = null;
+                cf.item_12 = GameManager.instance.defaultItem;
+                cf.item_12Much = 0;
+            }else if (slot == 2)
+            {
+                cf.item_01GameObject = null;
+                cf.item_01 = GameManager.instance.defaultItem;
+                cf.item_01Much = 0;
+            }else if (slot == 3)
+            {
+                cf.item_11GameObject = null;
+                cf.item_11 = GameManager.instance.defaultItem;
+                cf.item_11Much = 0;
+            }
         }
     }
 }
--- DragDropItem.cs
+++ DragDropItem.cs
@@ -1,9 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
+/*
+ *
+ *
+ *El objeto tiene que ser hijo dle canvas si no las escalas no funcionan
+ * 
+ */
 public class DragDropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
     public Canvas canvas;
@@ -19,22 +27,39 @@ public class DragDropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandle
 
     public CraftingManager cf;
 
+    public TMP_Text texto;
+
+    public Image imagen;
+
+    private bool lastWasCraftingSlot = false;
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
+
+        imagen.sprite = item.sprite;
+
+        cf = FindObjectOfType<CraftingManager>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        transform.SetAsLastSibling();
+        
         //canvasGroup.alpha = 0.6f;
         canvasGroup.blocksRaycasts = false;
-        startedPosition = rectTransform.anchoredPosition;
+        resetStartPosition();
 
         if (isOutput)
         {
             cf.outputDone();
             isOutput = false;
+        }
+
+        if (lastWasCraftingSlot)
+        {
+            cf.updateItems(eventData.pointerDrag);
         }
     }
 
@@ -47,17 +72,69 @@ public class DragDropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandle
             Slot slot = eventData.pointerEnter.transform.parent.GetComponent<Slot>();
             if (slot == null)
             {
-                rectTransform.anchoredPosition = startedPosition;
+                DragDropItem otherItem = eventData.pointerEnter.transform.parent.GetComponent<DragDropItem>();
+
+                if (otherItem == null)
+                {
+                    //Debug.Log("0");
+                    rectTransform.anchoredPosition = startedPosition;
+                }else{
+                    if (otherItem.item == item)
+                    {
+                        //Debug.Log("1");
+                        if (otherItem.itemMuch+itemMuch <= item.maxStack)
+                        {
+                            //Debug.Log("2");
+                            otherItem.sumarCantidad(itemMuch);
+                            borrarItem();
+                        }
+                        else
+                        {
+                            //Debug.Log("3");
+                        
+                            int x = item.maxStack - (otherItem.itemMuch+itemMuch);
+
+                            if (otherItem.itemMuch < itemMuch)
+                            {
+                                otherItem.itemMuch = otherItem.item.maxStack;
+                        
+                                actualizarCantidad(-x);
+                                otherItem.actualizarCantidad(item.maxStack);
+                            }
+                            else
+                            {
+                                itemMuch = item.maxStack;
+                        
+                                otherItem.actualizarCantidad(-x);
+                                actualizarCantidad(item.maxStack);
+                            }
+                        
+                            rectTransform.anchoredPosition = startedPosition;
+                        }
+                        
+                        //Updateamos aqui los slots tambien porque si juntamos 2 bloques dentro del craft no los detecta
+                        cf.update(eventData);
+                    }
+                    else
+                    {
+                        otherItem.resetStartPosition();
+                    
+                        RectTransform otherItemTransform = otherItem.gameObject.GetComponent<RectTransform>();
+                        otherItemTransform.anchoredPosition = startedPosition;
+
+                        rectTransform.anchoredPosition = otherItem.startedPosition;
+
+                        resetStartPosition();
+                    }
+                }
             }
             else
             {
                 Type t = slot.GetType();
                 
-                //Logica de añadir los items borrarlos del anterior o devolverlos etc
-                
                 if (t.Equals(typeof(CraftingSlot)))
                 {
-
+                    lastWasCraftingSlot = true;
                 }
             }
         }
@@ -67,6 +144,11 @@ public class DragDropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandle
         }
         
         canvasGroup.blocksRaycasts = true;
+    }
+
+    public void resetStartPosition()
+    {
+        startedPosition = rectTransform.anchoredPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -97,5 +179,23 @@ public class DragDropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandle
     public void actualizarCantidad(int x)
     {
         itemMuch = x;
+        texto.text = itemMuch.ToString();
+    }
+
+    public void sumarCantidad(int x)
+    {
+        itemMuch += x;
+        texto.text = itemMuch.ToString();
+    }
+    
+    public void restarCantidad(int x)
+    {
+        itemMuch -= x;
+        texto.text = itemMuch.ToString();
+    }
+
+    public void borrarItem()
+    {
+        Destroy(this.gameObject);
     }
 }
