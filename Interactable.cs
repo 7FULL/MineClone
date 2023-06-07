f7548f627618000e0d18e430e17aafc07ebdd5a6
7FULL <phgfull@gmail.com>
2023-06-07 09:21:35 +0200

    Prueba

--- Interactable.cs
+++ Interactable.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public interface Interactable
 {
+    //Fucnion a utilizar por todos los objetos interactuables
     public void interact(Vector3Int posicion = new Vector3Int(), RaycastHit hit = default);
 }
